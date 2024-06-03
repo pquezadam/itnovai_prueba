1c59d1d5c8d4fbc1985c046e9f9aa784209995b9
Pablo Quezada <pquezada@MacBook-Air-de-Pablo.local>
2024-06-03 16:02:22 -0400

    backend-index

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
+const productRoutes = require('./routes/products');
+const categoryRoutes = require('./routes/categories');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server is running in port 3000')
+        });
+    })
+    .catch(err => onslotchange.log(err));
